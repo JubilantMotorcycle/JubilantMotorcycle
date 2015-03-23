25527778d1f60b07914f70769920e4914be6f7ca
Steve <stephendwebster@gmail.com>
2015-03-23 11:08:26 -0700

    Add libraries for testing to package.json and gulpfile.js

--- tastebud/gulpfile.js
+++ tastebud/gulpfile.js
@@ -6,6 +6,10 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var mocha = require('gulp-mocha');
+var chai = require('chai');
+var chaiGulpHelpers = require('chai-gulp-helpers');
+chai.use(chaiGulpHelpers);
 
 var paths = {
   sass: ['./scss/**/*.scss']
